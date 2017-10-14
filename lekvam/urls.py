cd8d293971f6ed5757a2bdc6ccaf193a015100ba
Torvald Lekvam <torvald@lekvam.no>
2017-10-14 19:18:57 +0200

    Add urls for wiki, redirect to start node

--- lekvam/urls.py
+++ lekvam/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'common.views.home', name='home'),
+    # Examples
+    #url(r'^$', 'common.views.home', name='wiki_start'),
+    url(r'^$', RedirectView.as_view(url='/wiki/start')),
+    url(r'^wiki/', include('wiki_wrapper.urls')),
     url(r'^oppskrifter/', include('oppskrifter.urls')),
     url(r'^webcam/', include('webcam.urls')),
     url(r'^admin/', include(admin.site.urls)),
--- wiki_wrapper/urls.py
+++ wiki_wrapper/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<slug>.*)$', views.node, name='node'),
+    url(r'^(?P<slug>.*)$', views.node, name='wiki_node'),
 ]
