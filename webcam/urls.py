53a42a1adb3f01745745d537a934ec486c347645
Torvald Lekvam <torvald@lekvam.no>
2017-10-24 17:21:42 +0200

    Fix links in webcam app

--- webcam/urls.py
+++ webcam/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^pano$', views.pano, name='webcam_pano'),
-    url(r'^pano/(?P<date>\d{4}-\d{2}-\d{2})$', views.pano, name='webcam_pano'),
+    url(r'^pano/(?P<date>\d{4}-\d{2}-\d{2})$', views.pano, name='webcam_pano_date'),
     url(r'^live$', views.live, name='webcam_live'),
 ]
