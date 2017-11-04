7e810ae9249c2e8ecec87956ee49c22057a27c5c
Torvald Lekvam <torvald@lekvam.no>
2017-11-04 19:05:09 +0100

    Fix bug with non-dataform ajax calls

--- oppskrifter/static/js/recipe.js
+++ oppskrifter/static/js/recipe.js
@@ -1,4 +1,10 @@
 function ajax(div, method, url, payload, callback) {
+    var processData = true;
+    var contentType = "application/x-www-form-urlencoded; charset=UTF-8";
+    if (payload && payload.constructor.name === "FormData") {
+        processData = false;
+        contentType = false; // lol, this is the "correct" way to remove the header
+    }
     function csrfSafeMethod(method) {
         // these HTTP methods do not require CSRF protection
         return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
@@ -13,6 +19,8 @@ function ajax(div, method, url, payload, callback) {
         type: method,
         url: url,
         data: payload,
+        processData: processData,
+        contentType: contentType,
         success: function(response) {
             div.html(response)
             callback();
@@ -30,7 +38,6 @@ function addIngredientsListeners() {
         ajax($('#recipe-ingredients'), "POST", "ingredients", payload, addIngredientsListeners);
     });
     $('.remove-ingredient-button').click(function() {
-        //payload = {'id': $(this).attr('data-id')};
         var id = $(this).attr('data-id');
         ajax($('#recipe-ingredients'), "DELETE", "ingredient/" + id + "/delete", null, addIngredientsListeners);
     });
@@ -38,13 +45,27 @@ function addIngredientsListeners() {
 
 function addStepsListeners() {
     $('#add-step-button').click(function() {
-        payload = {'desc': $('#step-desc').val()}
+
+        payload = new FormData();
+        //payload.append("image", ($('#step-image')[0].files[0])); // stupid html
+        payload.append("image", document.getElementById("step-image").files[0]);
+        payload.append("desc", $('#step-desc').val());
+        payload.append("weight", 0)
+
         ajax($('#recipe-steps'), "POST", "steps", payload, addStepsListeners);
     });
-
+    $('.remove-step-button').click(function() {
+        var id = $(this).attr('data-id');
+        ajax($('#recipe-steps'), "DELETE", "step/" + id + "/delete", null, addStepsListeners);
+    });
 }
 
 $( document ).ready(function() {
     addIngredientsListeners();
     addStepsListeners();
+    $(document).on('change', ':file', function() {
+        var input = $(this),
+        label = input.val().replace(/\\/g, '/').replace(/.*\//, '').slice(0,15) + "...";
+        $('#step-image-upload-text').html(label);
+    });
 });
