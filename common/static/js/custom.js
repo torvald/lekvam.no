0094ee6930ad2fb8436c113cfea49b1888b82d4c
Torvald Lekvam <torvald@lekvam.no>
2017-11-23 21:18:55 +0100

    Add stuff for better web app experience

--- common/static/js/custom.js
+++ common/static/js/custom.js
@@ -38,3 +38,18 @@ function ajax(div, method, url, payload, callback) {
         }
     });
 }
+
+$( document ).ready(function() {
+    // http://www.creativebloq.com/html5/12-html5-tricks-mobile-81412803
+    window.addEventListener("load", function() { window. scrollTo(0, 0); });
+    var body = document.documentElement;
+    if (body.requestFullscreen) {
+          body.requestFullscreen();
+    } else if (body.webkitrequestFullscreen) {
+          body.webkitrequestFullscreen();
+    } else if (body.mozrequestFullscreen) {
+          body.mozrequestFullscreen();
+    } else if (body.msrequestFullscreen) {
+          body.msrequestFullscreen();
+    }
+});
