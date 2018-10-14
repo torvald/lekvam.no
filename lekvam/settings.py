a48450279979e974ad791dcd6dc5c0848660877f
Torvald Lekvam <torvald@lekvam.no>
2018-10-14 18:57:50 +0200

    Upgrade django tweaks

--- lekvam/settings.py
+++ lekvam/settings.py
@@ -125,3 +125,19 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'todo/static'),
     os.path.join(BASE_DIR, 'uploads'),
 )
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
--- lekvam/urls.py
+++ lekvam/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import RedirectView
 
 from django.contrib.auth import views as auth_views
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples
     #url(r'^$', 'common.views.home', name='wiki_start'),
     url(r'^$', RedirectView.as_view(url='/wiki/start')),
@@ -16,4 +16,4 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
-)
+]
